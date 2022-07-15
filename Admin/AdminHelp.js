2159027326c344d108d7fe4d85ef178632a27fa3
Floris <florismelchers@gmail.com>
2022-07-15 11:50:10 +0200

    set a better footer for help commands

--- Admin/AdminHelp.js
+++ Admin/AdminHelp.js
@@ -2,9 +2,7 @@ const { prefix } = require('../config.json');
 const { MessageEmbed, Collection, Permissions } = require('discord.js');
 
 module.exports = {
-  //Geef de naam van de command
 	name: 'adminhelp',
-  //Geef de beschrijving van de command
 	description: 'list of admin commands.',
 	aliases: ['ahelp'],
 	usage: '[command name]',
@@ -16,17 +14,16 @@ module.exports = {
 
 		if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return message.channel.send("You don't have the permissions to execute this command.")
 
-    	//Als er geen arguments zijn meegegeven, geef dan een complete lijst van alle commands. Skip daarna de rest van de code
 		if (!args.length) {
 
 			const helpDesc = commands.map(command => '```' + command.name + ' : ' + command.function + '```').join('\n');
 
 			//EMBED
 			const embed = new MessageEmbed()
-			.setTitle('Here\'s a list of all my admin commands:')
+			.setTitle('Here\'s a list of all my commands:')
 			.setDescription(helpDesc)
 			.setColor('RANDOM')
-			.setFooter({text: '\nYou can send ${prefix}help [command name] to get info on a specific command!'});
+			.setFooter({text: '\nThis are all the commands. Use .info [command name] to get more info about a command.'});
 			return message.channel.send({embeds: [embed]});
 
 		}
--- Tickets/TicketHelp.js
+++ Tickets/TicketHelp.js
@@ -3,9 +3,7 @@ const { prefix } = require('../config.json');
 const { MessageEmbed, Collection, Permissions } = require('discord.js');
 
 module.exports = {
-  //Geef de naam van de command
 	name: 'tickethelp',
-  //Geef de beschrijving van de command
 	description: 'list of all my ticket commands.',
 	admin: true,
     function: 'Tickets',
@@ -18,16 +16,15 @@ module.exports = {
 
 		if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return message.channel.send("You don't have the permissions to execute this command.")
 
-    	//Als er geen arguments zijn meegegeven, geef dan een complete lijst van alle commands. Skip daarna de rest van de code
 		if (!args.length) {
 
 			const helpDesc = commands.map(command => '```' + command.name + ' : ' + command.function + '```').join('\n');
 			//EMBED
 			const embed = new MessageEmbed()
-			.setTitle('Here\'s a list of all my ticket commands:')
+			.setTitle('Here\'s a list of all my commands:')
 			.setDescription(helpDesc)
 			.setColor('RANDOM')
-			.setFooter({text: '\nYou can send ${prefix}help [command name] to get info on a specific command!'});
+			.setFooter({text: '\nThis are all the commands. Use .info [command name] to get more info about a command.'});
 			return message.channel.send({embeds: [embed]});
 
 		}
