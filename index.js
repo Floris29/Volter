1730b91dd7b4bcc546da21941c1ca910fff264d5
Floris <Florismelchers@gmail.com>
2022-10-06 16:45:22 +0200

    internet

--- index.js
+++ index.js
@@ -48,16 +48,17 @@ for (const file of Ticketfiles) {
 }
 
 
-var logMessage = message.author.toString() + ' used ' + commandName + ' in ' + message.channel.toString();
-if (args.length > 0) {
-logMessage += ' with the arguments:' + args
-console.log (`logged command by ${message.member.user.toString()} in ${message.guild.name}.`)
 
-const channel = client.channels.cache.get(1027589173718298675)
-channel.send({
-  content: (logMessage)
-})}
-
+interaction.guild.channels.cache.get('1027589173718298675').channels.send({
+	embeds: [
+			new MessageEmbed()
+			.setTitle(`**Log System**`)
+			.setDescription(message.author.toString() + ' used ' + commandName + ' in ' + message.channel.toString())
+			.setColor(`#00caff`)
+			.setFooter('System')
+			.setTimestamp(Date.now())
+		]
+})
 client.on('messageCreate', message => {
 
   	//Check of het bericht een prefix heeft en dat het het bericht niet van een bot af komt. Is dit zo, skip dan de rest van de code
